e82006cdb6eba937afe1aebc816ad3f1c1282b08
Darian Puka <pukadarian@gmail.com>
2024-02-13 23:17:35 -0600

    change date format

--- lib/src/utils/datetime_helper.dart
+++ lib/src/utils/datetime_helper.dart
@@ -19,6 +19,8 @@ class DateTimeHelper {
       "${convertToTwoChars(dt.second)}";
 
   /// Convert a string to a datetime from mm/dd/yyyy
-  static DateTime fromDate(String date) => DateTime.parse(
-      "${date.substring(6, 10)}-${date.substring(3, 5)}-${date.substring(0, 2)}");
+  static DateTime fromDate(String date) =>
+      DateTime.parse("${date.substring(0, 4)}-"
+          "${date.substring(5, 7)}-"
+          "${date.substring(8, 10)}");
 }
